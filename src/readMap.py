ed04dbc69af663660616fc48fa82be8fb0de3643
Akshay <akshaybj0221@gmail.com>
2018-04-18 19:40:54 -0400

    making class to use subscriber data and added one templete mapper python script

--- src/readMap.py
+++ src/readMap.py
@@ -5,50 +5,55 @@ from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import MapMetaData
 from geometry_msgs.msg import Pose
 
+class mapData:
+	def callback(self, data):
+	#    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)	
+		self.occupancyMap = data.data
+		self.occList = []
+		self.occList.append(occupancyMap)
+		return 
 
-
-def callback(data):
-#    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)	
-	occupancyMap = data.data
-#	print(occupancyMap)
-	print("here ")
-   
-def callback2(data):
-#    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.resolution)
-	res = data.resolution
-	w = data.width
-	h = data.height
-	o = data.origin
-	print("res: ",res, "     W: ", w, "    H: ", h,)
-	print( "o: ")
-	print(o)
+	def callback2(self, data):
+	#    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.resolution)
+		self.res = data.resolution
+		self.w = data.width
+		self.h = data.height
+		self.o = data.origin
+	#	print("res: ",res, "     W: ", w, "    H: ", h,)
+	#	print( "o: ")
+	#	print(o)
 	
-	'''
-	occW = []
-	occH = []
-	count = 0
 
-	for i in occupancyMap:
-		count+=1
-		if (count <= w):
-			accW.append(i)
-		if (count > w):
-			accH.append(i)
+	def gridConversion(self, list1, list2):
+		'''
+		occW = []
+		occH = []
+		count = 0
+	
+		for i in occupancyMap:
+			count+=1
+			if (count <= w):
+				accW.append(i)
+			if (count > w):
+				accH.append(i)
+	
+		print("Width List: ", accW)
+		print("Height List: ", accH)
+	 	'''
 
-	print("Width List: ", accW)
-	print("Height List: ", accH)
- 	'''
+	def map_listener(self):
 
-def map_listener():
+		self.rospy.init_node('map_listener', anonymous=True) 
+		#initialze node named"map_listener"
+	
+		self.rospy.Subscriber("/map", OccupancyGrid, callback) 
+		#subscribe to /map	
 
-	rospy.init_node('map_listener', anonymous=True) #initialze node named "map_listener"
+		self.rospy.Subscriber("/map_metadata", MapMetaData, callback2)			
 
-#	occupancyMap = rospy.Subscriber("/map", OccupancyGrid, callback) #subscribe to /map
-
-	dimensionMap = rospy.Subscriber("/map_metadata", MapMetaData, callback2)	
-	print("dimension map height", dimensionMap[0])
-
-	rospy.spin()   # spin() simply keeps python from exiting until this node is stopped
+	#	print("dimension map res", dimensionMap)
+	
+		self.rospy.spin()   # spin() simply keeps python from exiting until this node is stopped
 
 
 
